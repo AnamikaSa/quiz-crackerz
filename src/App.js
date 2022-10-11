bb0db2e8faa3c953a4117a45c830f8ae65e251a2
AnamikaSa <amanika0061@gmail.com>
2022-10-11 10:21:07 +0600

    setup the router

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+  const router= createBrowserRouter([
+
+  ])
+
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;

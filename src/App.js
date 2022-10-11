edf25d851184028199b9f7901adb4d6c1e0a17e0
AnamikaSa <amanika0061@gmail.com>
2022-10-11 21:00:03 +0600

    header nav added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './componets/Home/Home';
+import Statistic from './componets/Statistic/Startstic';
+import Blog from './componets/Blog/Blog';
 
 function App() {
   const router= createBrowserRouter([
-
+    {
+      path: '/',
+      element: <Main></Main>,
+      children:[
+        {path: '/home', element:<Home></Home>},
+        {path: '/statistic', element:<Statistic></Statistic>},
+        {path: '/blog' , element: <Blog></Blog>}
+      ]
+    }
   ])
 
   return (

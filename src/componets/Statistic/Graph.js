1c1092516f214b485f3a4150993b0167b324bbb0
AnamikaSa <amanika0061@gmail.com>
2022-10-12 23:39:13 +0600

    graph added

--- src/componets/Statistic/Graph.js
+++ src/componets/Statistic/Graph.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bar, BarChart, LineChart,Line, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, LineChart,Line, Tooltip, XAxis, YAxis, Legend } from 'recharts';
 
 const Graph = ({statistic}) => {
     const{total, name}=statistic;
@@ -14,14 +14,15 @@ const Graph = ({statistic}) => {
     return (
         <div>
 
-            {/* < BarChart width={600} height={400} data={data}>
+            < BarChart width={600} height={400} data={data}>
                     <Line type="monotone" className='p-5 m-5' dataKey="t" stroke="#8884d8" tick={data} />
                     <XAxis dataKey="n" className='p-5 m-5' />
                     <YAxis dataKey="t" className='p-5 m-5' />
                     <Bar dataKey="t" className='p-5 m-5' barSize={20} fill="#8884d8"
                         label={data} />
                     <Tooltip></Tooltip>
-                </BarChart> */}
+                    <Legend/>
+                </BarChart>
 
         </div>
     );
--- src/componets/Statistic/Statistic.js
+++ src/componets/Statistic/Statistic.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Graph from './Graph';
 
+
 const Statistic = () => {
     const s=useLoaderData();
     return (
         <div>
             {
-                s.data.map(statistic=> <Graph
+                s.data.map (statistic=> <Graph
                 key= {statistic.id}
                 statistic={statistic}
                 ></Graph>)
